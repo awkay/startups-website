ee2673325b3a23b65d0a5ba7c59ac9c1d71328ce
Tony Kay <tony.kay@gmail.com>
2018-01-18 13:13:42 -0800

    updated versions
    Took 36 minutes

--- project.clj
+++ project.clj
@@ -9,7 +9,7 @@
                  ; ui
                  [cljsjs/semantic-ui-react "0.73.0-0"]
                  [fulcrologic/semantic-ui-react-wrappers "1.0.0"]
-                 [fulcrologic/fulcro "2.1.1-SNAPSHOT"]
+                 [fulcrologic/fulcro "2.1.2"]
                  [cljsjs/react-dom-server "15.6.2-1"]
                  [fulcrologic/fulcro-css "2.0.0"]
 
